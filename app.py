f352b0a25dd753fc69ff740af3a3a0d30789a49a
botirjon13 <tomama-uz@mail.ru>
2026-01-07 15:00:11 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ app = Flask(__name__, static_folder='webapp')
 
 PORT = int(os.environ.get("PORT", 5000))
 
+# /game route index.html qaytaradi
 @app.route("/game")
 def game():
     return send_from_directory('webapp', 'index.html')
 
+# boshqa static fayllar uchun
 @app.route("/<path:path>")
 def static_proxy(path):
     return send_from_directory('webapp', path)
